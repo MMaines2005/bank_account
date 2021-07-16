8313f34988be9052edc20bb6e6f92a4e754bdef2
Michael A Maines <85315191+MMaines2005@users.noreply.github.com>
2021-07-16 06:25:22 -0400

    Class setup

---
+++ bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+
+    def __init__(self, int_rate,balance):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def display_account_info(self):
+        pass
+
+    def yield_interest(self):
+        pass
